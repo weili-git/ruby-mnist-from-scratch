d2a109cbc20020225af6e188c9c84cbd9560676d
weili-git <244385319@qq.com>
2020-07-21 14:00:09 +0800

    Update demo.rb

--- demo.rb
+++ demo.rb
@@ -10,7 +10,7 @@ module NnFunctions
     x2.exp / x2.exp.sum(1)[0]
   end
 
-  def argmax(x)
+  def argmax(x)  # built-in NMatrix
     list = x.shape[0] == 1 ? [x.to_a] : x.to_a
     res = list.map do |row|
       max = -Float::MAX
@@ -67,7 +67,11 @@ module NnFunctions
     -(y*(x+1e-7).log).sum.sum(1)[0, 0] / batch_size
   end
 
-  def numerical_diff(f, x)  # numerical_diff(method(:f), x)  # https://www.jianshu.com/p/139037ac0b31
+  def numerical_diff(f, x)
+    (f.call(x+1e-4) - f.call(x-1e-4))/2e-4
+  end
+
+  def numerical_gradient_1d(f, x)  # numerical_gradient(method(:f), x)  # https://www.jianshu.com/p/139037ac0b31
     h = 1e-4
     if x.dtype == :int32
       x = x.cast(:list, :float64)
@@ -85,6 +89,34 @@ module NnFunctions
     return grad
   end
 
+  def numerical_gradient_2d(f, x)
+    if x.dim == 1
+      numerical_gradient_1d(f, x)
+    else
+      grad = N::zeros_like(x)
+      x.each_with_index do |xx, i|
+        x[i] = numerical_gradient_1d(f, xx)
+      end
+    end
+  end
+
+  def numerical_gradient(f, x)
+    h = 1e-4
+    # grad = M::zeros_like(x)
+    len = 1
+    x.shape.each do |s|
+      len = len*s
+    end
+    grad = NMatrix.new([len], 0)
+    #
+    x.each_with_index do |xx, i|
+      fx1 = f.call(xx+h)
+      fx2 = f.call(xx-h)
+      grad[i] = (fx1-fx2)/(2*h)  # 如何赋值对应
+    end
+    return grad.reshape(x.shape)
+  end
+
   def gradient_descent(f, init_x, lr=1e-2, step_num=100)
     if init_x.dtype == :int32
       init_x = init_x.cast(:list, :float64)
@@ -97,3 +129,100 @@ module NnFunctions
     return x
   end
 end
+
+class SimpleNet
+  def initialize(in_dim, out_dim)
+    @w = NMatrix.random([in_dim, out_dim])
+  end
+  def predict(x)
+    x.dot @w
+  end
+  def loss(x, t)
+    z = predict(x)
+    y = softmax(z)
+    mean_squared_error(y, t)
+  end
+end
+
+
+class TwoLayerNet
+  attr_reader :params
+  def initialize(in_dim, hid_dim, out_dim, std=0.01)
+    @params = Hash.new
+    @params['W1'] = NMatrix.random([in_dim, hid_dim]) * std
+    @params['b1'] = NMatrix.new([1, hid_dim], 0.0)
+    @params['W2'] = NMatrix.random([hid_dim, out_dim]) * std
+    @params['b2'] = NMatrix.new([1, out_dim], 0.0)
+  end
+
+  def predict(x)
+    w1, w2 = @params['W1'], @params['W2']
+    b1, b2 = @params['b1'], @params['b2']
+    a1 = x.dot(w1) + b1.repeat(x.shape[0], 0)  # batch
+    z1 = sigmoid(a1)
+    a2 = z1.dot(w2)+b2.repeat(x.shape[0], 0)
+    y = softmax(a2)
+    return y
+  end
+
+  def loss(x, t)
+    y = predict(x)
+    return cross_entropy_error(y, t)
+  end
+
+  def accuracy(x, t)
+    y = predict(x)
+    y = argmax(y)
+    t = argmax(t)
+    # accuracy = (y==t)
+    total = y.shape[0]
+    accurate = 0.0
+    total.times do |idx|
+      if y[idx] == t[idx]
+        accurate += 1.0
+      end
+    end
+    return accurate / total.to_f
+  end
+
+  def num_grad(x, t)  # error!
+    loss_w = lambda {|w| loss(x, t)}
+    grads = Hash.new
+    grads['W1'] = numerical_gradient(method(:loss_w), @params['W1'])
+    grads['b1'] = numerical_gradient(method(:loss_w), @params['b1'])
+    grads['W2'] = numerical_gradient(method(:loss_w), @params['W2'])
+    grads['b2'] = numerical_gradient(method(:loss_w), @params['b2'])
+    return grads
+  end
+end
+# ######################################################
+include NnFunctions
+# t = N[0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+# y = N[0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
+# t = N[[0, 0, 1, 0, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]]
+# y = N[[0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0], [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]]
+# p "mse:" , mean_squared_error(y, t)
+# p "cee:" , cross_entropy_error(y, t)
+# gets
+# ######################################################
+# ######################################################
+# def f(x)
+#   return x[0]**2 + x[1]**2
+# end
+# x = N[3.0, 4.0]
+# p numerical_gradient_1d(method(:f), x).to_s
+# p gradient_descent(method(:f), x, 0.1).to_s
+# ######################################################
+# ######################################################
+# w = SimpleNet.new(2, 3)
+# x = N[[0.6, 0.9]]
+# p argmax(w.predict(x)).to_s  # argmax
+# p w.loss(x, N[[1.0, 0.0, 0.0]])
+# ######################################################
+# ######################################################
+net = TwoLayerNet.new(28*28, 100, 10)  # net
+x = NMatrix.random([100, 28*28])  # image
+t = NMatrix.random([100, 10])  # label
+pp net.predict(x).shape
+pp net.loss(x, t)
+# pp net.num_grad(x, t).shape
